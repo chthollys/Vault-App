72d980872888a7ca1bf409dee85d963824be6c7d
chthollys <jason130105@gmail.com>
2025-07-23 14:36:35 +0700

    Created Definition
    Signed-off-by: chthollys <jason130105@gmail.com>

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { ComponentPropsWithoutRef } from "react";
 
-export interface ChildrenProps {
+export interface ChildrenProp {
   children?: React.ReactNode;
+}
+
+export interface LinkSectionProps extends ChildrenProp {
+  label: string | undefined;
+  labelClass?: string | undefined;
+  sectionClass?: ComponentPropsWithoutRef<"div">["className"];
+}
+
+export interface LinkListProps extends ChildrenProp {
+  className?: ComponentPropsWithoutRef<"ul">["className"];
+}
+
+export interface LinkItemProps extends ChildrenProp {
+  href: string;
 }
