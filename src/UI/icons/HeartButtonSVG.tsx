ce66b9a7bea16526910bc96599ffa8392f0e4434
chthollys <jason130105@gmail.com>
2025-07-23 18:57:07 +0700

    Added HeartSVG icon for GameCard component
    Signed-off-by: chthollys <jason130105@gmail.com>

---
+++ src/UI/icons/HeartButtonSVG.tsx
@@ -0,0 +1,27 @@
+import classes from "./HeartButtonSVG.module.css";
+import { HeartButtonSVGProps } from "~/lib/definitions";
+
+export default function HeartButtonSVG({
+  id,
+  active = false,
+  onClick,
+}: HeartButtonSVGProps) {
+  return (
+    <button
+      type="button"
+      className={`${classes["wishlist-btn"]} ${active ? "active" : ""}`}
+      data-produk-id={id}
+      onClick={onClick}
+      aria-label={active ? "Remove from wishlist" : "Add to wishlist"}
+    >
+      <svg
+        className={classes["heart-icon"]}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+      >
+        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+      </svg>
+    </button>
+  );
+}
