0d0fc7ae68be6da394b0d77239be1fc0fb5ea062
chthollys <jason130105@gmail.com>
2025-07-23 14:35:48 +0700

    Create Link UI compound
    Signed-off-by: chthollys <jason130105@gmail.com>

---
+++ src/UI/LinkSection/LinkList.tsx
@@ -0,0 +1,5 @@
+import { LinkListProps } from "~/lib/definitions";
+
+export default function LinkList({ className, children }: LinkListProps) {
+  return <ul className={className}>{children}</ul>;
+}
