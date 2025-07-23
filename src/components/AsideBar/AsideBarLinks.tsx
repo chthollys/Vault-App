79f3da51962906243c63b5e2c2c8f0a57911e2f1
chthollys <jason130105@gmail.com>
2025-07-23 14:37:08 +0700

    Created AsideBar Component
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/components/AsideBar/AsideBarLinks.tsx
+++ src/components/AsideBar/AsideBarLinks.tsx
@@ -1,6 +1,7 @@
-import { ChildrenProps } from "~/lib/definitions";
+import { LinkListProps } from "~/lib/definitions";
+import LinkList from "@/UI/LinkSection/LinkList";
 import classes from "./AsideBar.module.css";
 
-export default function AsideBarLinks({ children }: ChildrenProps) {
-  return <ul className={classes["category-list"]}>{children}</ul>;
+export default function AsideBarLinks({ children }: LinkListProps) {
+  return <LinkList className={classes["category-list"]}>{children}</LinkList>;
 }
