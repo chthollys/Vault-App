8312c105cbed3ecd5dcb0868bd1e85afb765a03b
chthollys <jason130105@gmail.com>
2025-07-25 20:27:13 +0700

    Added Tanstack Query package
    Signed-off-by: chthollys <jason130105@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { tr } from "zod/locales";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_S3_REGION}.amazonaws.com`,
+        port: "",
+        pathname: `/game-cover/**`,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
