f25f5d66f0b8d15d2f5e36aab98d409dc4686563
chthollys <jason130105@gmail.com>
2025-07-28 18:16:24 +0700

    Added custom HydrationBoundary for fetching games
    Signed-off-by: chthollys <jason130105@gmail.com>

---
+++ src/app/hydration.tsx
@@ -0,0 +1,18 @@
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { getQueryClient } from "@/lib/utils/get-query-client";
+import { getGames } from "./actions";
+import { ChildrenProp } from "@/lib/definitions";
+
+export default async function Hydration({ children }: ChildrenProp) {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["games"],
+    queryFn: getGames,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {children}
+    </HydrationBoundary>
+  );
+}
