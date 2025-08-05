74fa1df9f3b5af592cdb795c9c7cbf39abe0c822
chthollys <jason130105@gmail.com>
2025-08-05 21:13:33 +0700

    Added new action function
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,6 +2,7 @@
 
 import db from "~/prisma/db";
 import bcrypt from "bcrypt";
+import { CreateReviewData, CreateUserData, User } from "@/lib/types/data";
 
 export async function getUsers() {
   return await db.user.findMany({});
@@ -31,6 +32,43 @@ export async function getGenreByGameId(gameId: string) {
   return response;
 }
 
+export const getReviewByGameId = async (gameId: string) => {
+  const response = await db.review.findMany({
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return response;
+};
+
+export const getUserByReviewId = async (reviewId: string) => {
+  const response = await db.review.findUnique({
+    where: {
+      id: reviewId,
+    },
+    select: {
+      user: {
+        select: {
+          username: true,
+          profile: {
+            select: {
+              avatarUrl: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return {
+    avatarUrl: response?.user.profile?.avatarUrl ?? null,
+    reviewer: response?.user.username ?? null,
+  };
+};
+
 export const saveUserPassword = async (
   userId: string,
   plainPassword: string
@@ -54,4 +92,25 @@ export const verifyPassword = async (
   });
   if (!user) return false;
   return bcrypt.compare(inputPassword, user.password);
+};
+
+export const createUser = async (user: CreateUserData) => {
+  const hashedPassword = await bcrypt.hash(user.password, 13);
+  const response = await db.user.create({
+    data: {
+      ...user,
+      password: hashedPassword,
+      profile: {
+        create: {},
+      },
+    },
+  });
+  return response;
+};
+
+export const createReview = async (review: CreateReviewData) => {
+  const response = await db.review.create({
+    data: review,
+  });
+  return response;
 };
