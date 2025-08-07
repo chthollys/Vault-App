d5bd74700381e3add3971150ac75ec844aab082c
chthollys <jason130105@gmail.com>
2025-08-07 18:01:35 +0700

    remove hydration boundary
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/games/all/page.tsx
+++ src/app/games/all/page.tsx
@@ -1,12 +1,11 @@
 import MainGrid from "./components/MainGrid";
-import Hydration from "@/app/hydration";
 import Title from "./components/Title";
 
 export default async function AllGamesPage() {
   return (
-    <Hydration>
+    <>
       <Title />
       <MainGrid />
-    </Hydration>
+    </>
   );
 }
