92f71c92d0177b1d5ce62a366b993f16bee76ed4
chthollys <jason130105@gmail.com>
2025-08-11 15:28:57 +0700

    Added genres prefetchQuery
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/hydration.tsx
+++ src/app/hydration.tsx
@@ -1,14 +1,20 @@
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { getQueryClient } from "@/lib/utils/get-query-client";
-import { getGames } from "./actions";
+import { getGames, getGenres } from "./actions";
 import type { ChildrenProp } from "@/lib/types/props";
 
 export default async function Hydration({ children }: ChildrenProp) {
   const queryClient = getQueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["games"],
-    queryFn: () => getGames(),
-  });
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: ["games"],
+      queryFn: () => getGames(),
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ["genres"],
+      queryFn: getGenres,
+    }),
+  ]);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
