84b9b509736f9e1a67f95beafc339ed091728efc
chthollys <jason130105@gmail.com>
2025-08-12 08:44:40 +0700

    Optimize field db
    Signed-off-by: chthollys <jason130105@gmail.com>

--- prisma/migrations/20250728031154_add_rating_field_game/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Game" ADD COLUMN     "rating" DOUBLE PRECISION NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "Genre" ADD COLUMN     "parentId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Genre" ADD CONSTRAINT "Genre_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Genre"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20250808113807_added_discounted_price_and_status_default_value_in_order/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Game" ADD COLUMN     "discountedPrice" DOUBLE PRECISION;
-
--- AlterTable
-ALTER TABLE "public"."Order" ALTER COLUMN "status" SET DEFAULT 'PENDING';
-
--- AlterTable
-ALTER TABLE "public"."Profile" ALTER COLUMN "avatarUrl" SET DEFAULT 'https://vault-app-bucket.s3.ap-southeast-2.amazonaws.com/default-cover/user-default.jpg';
