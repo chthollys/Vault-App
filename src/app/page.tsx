2c404388f83797b39245bab503c797b8a5d842c9
chthollys <jason130105@gmail.com>
2025-08-12 16:10:31 +0700

    Rename fo Page
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import GameGridsSection from "./components/GameGridsSection";
 import FeaturedGames from "./components/FeaturedGames";
 import Hydration from "./hydration";
 
-export default function Home() {
+export default function Page() {
   return (
     <Hydration>
       <FeaturedGames />
