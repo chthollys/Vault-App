7411f29822d1c72a02c1dc9e296a4675ff7444a7
chthollys <jason130105@gmail.com>
2025-08-14 18:13:51 +0700

    Fix error: useGamesInfinite must be prefetched in hydration
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/hydration.tsx
+++ src/app/hydration.tsx
@@ -1,23 +1,24 @@
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { getQueryClient } from "@/lib/utils/get-query-client";
-import { getGames, getGenres } from "./actions";
+import { getGames, getGamesPaginated, getGenres } from "./actions";
 import type { HydrationProps } from "@/lib/types/props";
-
-const sortRules = {
-  categories: [],
-  sortBy: null,
-};
 
 export default async function Hydration({ children }: HydrationProps) {
   const queryClient = getQueryClient();
   await Promise.all([
     queryClient.prefetchQuery({
-      queryKey: ["games", sortRules.categories, sortRules.sortBy],
-      queryFn: () => getGames(sortRules),
+      queryKey: ["games", null, null],
+      queryFn: () => getGames(null),
     }),
     queryClient.prefetchQuery({
       queryKey: ["genres"],
       queryFn: getGenres,
+    }),
+    queryClient.prefetchInfiniteQuery({
+      queryKey: ["games", "infinite", null, null],
+      queryFn: ({ pageParam = 0 }) =>
+        getGamesPaginated(null, pageParam as number, 10),
+      initialPageParam: 0,
     }),
   ]);
 
