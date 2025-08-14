e4bd4597f7c60f31a031ae4f19525b8d29380f9a
chthollys <jason130105@gmail.com>
2025-08-14 18:48:42 +0700

    Fix SSR bug: wrap page component inside of HydrationBoundary
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/games/all/page.tsx
+++ src/app/games/all/page.tsx
@@ -1,11 +1,14 @@
+import Hydration from "@/app/hydration";
 import MainGrid from "./components/MainGrid";
 import { TitleSection } from "./components/Title";
 
 export default async function AllGamesPage() {
   return (
     <>
-      <TitleSection />
-      <MainGrid />
+      <Hydration>
+        <TitleSection />
+        <MainGrid />
+      </Hydration>
     </>
   );
 }
