26473b07a6bf416744402ba7e8e13e34cbbbc872
chthollys <jason130105@gmail.com>
2025-08-16 20:27:33 +0700

    barrel export
    Signed-off-by: chthollys <jason130105@gmail.com>

---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,11 @@
+import GameCardInfoWrapper from "./GameCardInfoWrapper";
+import GameCardWrapper from "./GameCardWrapper";
+import GameCoverWrapper from "./GameCoverWrapper";
+import SectionHeaderWrapper from "./SectionHeaderWrapper";
+
+export {
+  GameCardInfoWrapper,
+  GameCardWrapper,
+  GameCoverWrapper,
+  SectionHeaderWrapper,
+};
