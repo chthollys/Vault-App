ca19af702435cd2f83d0675e0745076004c70b1e
chthollys <jason130105@gmail.com>
2025-08-16 11:08:53 +0700

    Add Wrapper
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/components/Typography/Logo.tsx
+++ src/components/Typography/Logo.tsx
@@ -1,9 +1,19 @@
-import type { ChildrenProp } from "@/lib/types/props";
+import type { SpanElementProps } from "@/lib/types/props";
+import { Wrapper } from "../Wrapper/base";
 
-export default function Logo({ children }: ChildrenProp) {
+export default function Logo({
+  className,
+  children,
+  ...props
+}: SpanElementProps) {
   return (
-    <span className="bg-logo bg-clip-text text-[1.4rem] font-extrabold text-transparent">
+    <Wrapper
+      as="span"
+      className="bg-logo bg-clip-text text-[1.4rem] font-extrabold text-transparent"
+      nextClass={className}
+      {...props}
+    >
       {children}
-    </span>
+    </Wrapper>
   );
 }
