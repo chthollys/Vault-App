136142684bdeb2dda471c58f37818107fc103ea8
chthollys <jason130105@gmail.com>
2025-08-18 14:55:57 +0700

    merge nav enchance
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import { Logo } from "../Typography";
 import Menu from "./Menu";
+import { UserAvatarIcon } from "@/UI/icons";
+import { auth } from "@/app/actions/auth";
+import { LoginButton } from "@/UI/buttons";
 
-export default function MainHeader() {
+export default async function MainHeader() {
+  const session = await auth();
   return (
     <nav
-      className="backdrop-blur-[20px] border-glass-border shadow-glass sticky top-0 z-50 border-b-[1px] border-solid bg-[rgba(13,_17,_23,_0.85)]"
+      className="border-glass-border shadow-glass sticky top-0 z-50 border-b-[1px] border-solid bg-[rgba(13,_17,_23,_0.85)] backdrop-blur-[20px]"
       role="navigation"
     >
       <div className="flex w-auto items-center justify-between gap-2 px-8 py-[0.375rem]">
-        <Link href={"/"} className="block mr-2">
+        <Link href={"/"} className="mr-2 block">
           <Logo>Vault</Logo>
         </Link>
 
@@ -19,12 +23,16 @@ export default function MainHeader() {
           {"SEARCH BAR IMPLEMENTATION"}
         </div>
 
-        <div className="nav-icons">
-          <div className="nav-icon">{"cart" /* {"VIEW SHOPPING CART"} */}</div>
-          <div className="nav-icon profile">
-            {
-              "profile" /* {'VIEW USER PROFILE'}  check if the user was logged in */
-            }
+        <div className="flex items-center gap-6">
+          <div className="relative">{"cart" /* {"VIEW SHOPPING CART"} */}</div>
+          <div className="">
+            {session ? (
+              <UserAvatarIcon imageUrl={session.user.image} />
+            ) : (
+              <Link href={"/login"}>
+                <LoginButton>Sign in</LoginButton>
+              </Link>
+            )}
           </div>
         </div>
       </div>
