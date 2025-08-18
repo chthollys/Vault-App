fa69f9e9e08ac23f17b1bc6e65cc21bc9c51b81a
chthollys <jason130105@gmail.com>
2025-08-18 17:05:03 +0700

    Added showIcon boolean
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/UI/buttons/SignInButton.tsx
+++ src/UI/buttons/SignInButton.tsx
@@ -2,13 +2,21 @@ import { Button, ButtonProps } from "@mui/material";
 import { SignInIcon } from "../icons";
 import { LoadingSpinner } from "../Spinner";
 
-export default function SignInButton({ children, ...props }: ButtonProps) {
+interface SignInButtonProps extends ButtonProps {
+  showIcon?: boolean;
+}
+
+export default function SignInButton({
+  showIcon = true,
+  children,
+  ...props
+}: SignInButtonProps) {
   return (
     <Button
       variant="contained"
       className="bg-primary! hover:bg-primary-dark! rounded-md py-3 text-[0.95rem] font-semibold normal-case"
       size="large"
-      startIcon={<SignInIcon />}
+      startIcon={showIcon ? <SignInIcon /> : null}
       loadingIndicator={
         <LoadingSpinner color="white" variant="simple" size="sm" />
       }
