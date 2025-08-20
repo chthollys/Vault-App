2ba2132e7490e120da77eb728313f5dc32120883
chthollys <jason130105@gmail.com>
2025-08-20 16:22:56 +0700

    hydration file
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,7 @@
 import Hydration from "../hydration";
 import HomePage from "./components/HomePage";
 import { Suspense } from "react";
-import Loading from "./loading";
+import Loading from "../loading";
 
 export default function Home() {
   return (
