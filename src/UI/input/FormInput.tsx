c3d2f98cc87710148c949c22847080646f30e4e5
chthollys <jason130105@gmail.com>
2025-08-20 09:38:51 +0700

    revert forwardRef usage, solved by spreading all props
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/UI/input/FormInput.tsx
+++ src/UI/input/FormInput.tsx
@@ -2,59 +2,50 @@
 
 import type { AuthFormInputProps } from "@/lib/types/props";
 import { Input } from "@heroui/react";
-import { forwardRef } from "react";
 
-const FormInput = forwardRef<HTMLInputElement, AuthFormInputProps>(
-  function FormInput(
-    {
-      name,
-      id = name,
-      label = "",
-      placeholder = "",
-      type = "text",
-      isInvalid,
-      errorMessage,
-      ...props
-    },
-    ref
-  ) {
-    return (
-      <Input
-        ref={ref}
-        id={id}
-        name={name}
-        label={label}
-        type={type}
-        size="lg"
-        labelPlacement="outside-top"
-        placeholder={placeholder}
-        isInvalid={isInvalid}
-        errorMessage={errorMessage}
-        {...props}
-        classNames={{
-          label: "text-[0.9rem] font-medium tracking-wide text-white/70",
-          input: ["bg-glass", "text-black/90 dark:text-white/90", "pl-4"],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "shadow-glass",
-            "bg-glass",
-            "hover:bg-glass-hover!",
-            "group-data-[focus=true]:bg-glass-active",
-            "group-data-[hover=true]:bg-glass-hover!",
-            "cursor-text!",
-            "border-glass-border",
-            "backdrop-glass",
-            "rounded-lg",
-            "border-[1px]",
-            "border-solid ",
-            "text-[0.95rem]",
-            "text-white/90",
-            "transition",
-          ],
-        }}
-      />
-    );
-  }
-);
-
-export default FormInput;
+export default function FormInput({
+  name,
+  id = name,
+  label = "",
+  placeholder = "",
+  type = "text",
+  isInvalid,
+  errorMessage,
+  ...props
+}: AuthFormInputProps) {
+  return (
+    <Input
+      id={id}
+      name={name}
+      label={label}
+      type={type}
+      size="lg"
+      labelPlacement="outside-top"
+      placeholder={placeholder}
+      isInvalid={isInvalid}
+      errorMessage={errorMessage}
+      {...props}
+      classNames={{
+        label: "text-[0.9rem] font-medium tracking-wide text-white/70",
+        input: ["bg-glass", "text-black/90 dark:text-white/90", "pl-4"],
+        innerWrapper: "bg-transparent",
+        inputWrapper: [
+          "shadow-glass",
+          "bg-glass",
+          "hover:bg-glass-hover!",
+          "group-data-[focus=true]:bg-glass-active",
+          "group-data-[hover=true]:bg-glass-hover!",
+          "cursor-text!",
+          "border-glass-border",
+          "backdrop-glass",
+          "rounded-lg",
+          "border-[1px]",
+          "border-solid ",
+          "text-[0.95rem]",
+          "text-white/90",
+          "transition",
+        ],
+      }}
+    />
+  );
+}
