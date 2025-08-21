51e20d93240ade027ab04728f2d928250598740a
chthollys <jason130105@gmail.com>
2025-08-21 13:47:41 +0700

    extend types in session and token
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/lib/types/next-auth.d.ts
+++ src/lib/types/next-auth.d.ts
@@ -1,9 +1,20 @@
-import { DefaultSession } from "next-auth";
+import type { DefaultSession, DefaultUser } from "next-auth";
+import "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
-    user: {
+    user: DefaultSession["user"] & {
       id: string;
-    } & DefaultSession["user"];
+    };
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
   }
 }
