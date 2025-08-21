6ad77236d5c0668956b5e938695351fad73486e6
chthollys <jason130105@gmail.com>
2025-08-21 14:22:23 +0700

    Added ToastProvider
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -5,6 +5,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import type { ChildrenProp } from "@/lib/types/props";
 import { getQueryClient } from "@/lib/utils/get-query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import ToastProvider from "@/components/ToastProvider";
 
 export default function Provider({ children }: ChildrenProp) {
   const queryClient = getQueryClient();
@@ -13,6 +14,7 @@ export default function Provider({ children }: ChildrenProp) {
     <QueryClientProvider client={queryClient}>
       <HeroUIProvider>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ToastProvider />
           {children}
         </AppRouterCacheProvider>
       </HeroUIProvider>
