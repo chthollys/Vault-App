77927d33d2f965268ca7e3475c572a2e0f49f6a6
chthollys <jason130105@gmail.com>
2025-08-22 17:58:34 +0700

    theme var: bg-nav-bar
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import { Logo } from "../Typography";
 import Menu from "./Menu";
-import { UserAvatarIcon } from "@/UI/icons";
 import { auth } from "@/auth";
 import { LoginButton } from "@/UI/buttons";
+import { UserActionModal } from "../UserActionModal";
 
 export default async function MainHeader() {
   const session = await auth();
   return (
     <nav
-      className="border-glass-border shadow-glass sticky top-0 z-50 border-b-[1px] border-solid bg-[rgba(13,_17,_23,_0.85)] backdrop-blur-[20px]"
+      className="border-glass-border shadow-glass bg-nav-bar sticky top-0 z-50 border-b-[1px] border-solid backdrop-blur-[20px]"
       role="navigation"
     >
       <div className="flex w-auto items-center justify-between gap-2 px-8 py-[0.375rem]">
@@ -27,7 +27,7 @@ export default async function MainHeader() {
           <div className="relative">{"cart" /* {"VIEW SHOPPING CART"} */}</div>
           <div className="">
             {session ? (
-              <UserAvatarIcon imageUrl={session.user.image} />
+              <UserActionModal iconUrl={session.user.image} />
             ) : (
               <Link href={"/login"}>
                 <LoginButton>Sign in</LoginButton>
