fef2156bc4a965b93fe83fc92345e79c46adab11
chthollys <jason130105@gmail.com>
2025-08-26 20:39:22 +0700

    remove hydration
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/(main)/games/all/page.tsx
+++ src/app/(main)/games/all/page.tsx
@@ -1,14 +1,11 @@
-import Hydration from "@/app/hydration";
 import MainGrid from "./components/MainGrid";
 import { TitleSection } from "./components/Title";
 
 export default async function AllGamesPage() {
   return (
     <>
-      <Hydration>
-        <TitleSection />
-        <MainGrid />
-      </Hydration>
+      <TitleSection />
+      <MainGrid />
     </>
   );
 }
