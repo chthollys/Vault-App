9cf2da69b6c3ce14542bd3a59c8882fd7d5068ec
chthollys <jason130105@gmail.com>
2025-08-27 21:40:43 +0700

    Add navigate roiter to HeroUIProvider
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -6,13 +6,15 @@ import type { ChildrenProp } from "@/lib/types/props";
 import { getQueryClient } from "@/lib/utils/get-query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import ToastProvider from "@/components/ToastProvider";
+import { useRouter } from "next/navigation";
 
 export default function Provider({ children }: ChildrenProp) {
   const queryClient = getQueryClient();
+  const router = useRouter();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <HeroUIProvider>
+      <HeroUIProvider navigate={router.push}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ToastProvider />
           {children}
