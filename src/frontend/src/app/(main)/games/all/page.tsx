a3f2a640e20d5b5592bac6545b964036dfd2b845
chthollys <jason130105@gmail.com>
2025-09-08 10:19:51 +0700

    fix(frontend-allgame/page): add div wrapper to avoid additional row in grid display
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/frontend/src/app/(main)/games/all/page.tsx
+++ src/frontend/src/app/(main)/games/all/page.tsx
@@ -3,9 +3,9 @@ import { TitleSection } from "./components/Title";
 
 export default async function AllGamesPage() {
   return (
-    <>
+    <div id="all-game-main-content">
       <TitleSection />
       <MainGrid />
-    </>
+    </div>
   );
 }
