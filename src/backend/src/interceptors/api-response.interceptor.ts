c4b1368de8a9fd3557c2a1bca8f9db5eb1d40bb1
chthollys <jason130105@gmail.com>
2025-09-12 21:21:10 +0700

    ft(backend): add api response interceptor
    Signed-off-by: chthollys <jason130105@gmail.com>

---
+++ src/backend/src/interceptors/api-response.interceptor.ts
@@ -0,0 +1,34 @@
+// src/interceptors/api-response.interceptor.ts
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from "@nestjs/common";
+import { Observable, map } from "rxjs";
+import type { ApiResponse } from "repo/types";
+
+@Injectable()
+export class ApiResponseInterceptor<T>
+  implements NestInterceptor<T, ApiResponse<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<ApiResponse<T>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        if (typeof data === "string") {
+          return { success: true, message: data } as ApiResponse;
+        }
+        if (data === undefined || data === null) {
+          return {
+            success: true,
+            message: "Operation successful",
+          } as ApiResponse;
+        }
+        return { success: true, data } as ApiResponse<T>;
+      }),
+    );
+  }
+}
