951e4e05695db7e1a9139111d6471abdad6fb2b1
chthollys <jason130105@gmail.com>
2025-09-12 21:05:30 +0700

    ft(backend): add exception filter
    Signed-off-by: chthollys <jason130105@gmail.com>

---
+++ src/backend/src/filters/api-exception.filter.ts
@@ -0,0 +1,36 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
+import { Response } from "express";
+import type { ApiResponse } from "repo/types";
+
+@Catch()
+export class ApiExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = "Internal server error";
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const res = exception.getResponse();
+      message =
+        typeof res === "string"
+          ? res
+          : (res as any).message || exception.message;
+    }
+
+    const errorResponse: ApiResponse = {
+      success: false,
+      message,
+    };
+
+    response.status(status).json(errorResponse);
+  }
+}
