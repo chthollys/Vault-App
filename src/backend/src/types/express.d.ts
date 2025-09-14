7d940b33723623ca17fd075cb912cccdc4698244
chthollys <jason130105@gmail.com>
2025-09-14 19:55:22 +0700

    ft(backend/types): extend Session interface for auth flow
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/backend/src/types/express.d.ts
+++ src/backend/src/types/express.d.ts
@@ -1,8 +1,13 @@
 import type { AuthUser } from "src/auth/types/payload";
 import "express";
+import type { UserSignupStepData } from "repo/types";
 
 declare module "express" {
   interface Request {
     user?: AuthUser;
+  }
+
+  interface Session {
+    "signup-state"?: UserSignupStepData;
   }
 }
