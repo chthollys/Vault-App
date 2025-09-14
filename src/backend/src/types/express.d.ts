3a1e4777691d48007feb8751ff9c59d9d6a741a9
chthollys <jason130105@gmail.com>
2025-09-14 21:48:43 +0700

    refactor(backend/types): add separate module for express session
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/backend/src/types/express.d.ts
+++ src/backend/src/types/express.d.ts
@@ -1,12 +1,15 @@
 import type { AuthUser } from "src/auth/types/payload";
 import "express";
+import "express-session";
 import type { UserSignupStepData } from "repo/types";
 
 declare module "express" {
   interface Request {
     user?: AuthUser;
   }
+}
 
+declare module "express-session" {
   interface Session {
     "signup-state"?: UserSignupStepData;
   }
