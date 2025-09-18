454dbfebf6ff589c0de40a263271100a616984e0
chthollys <jason130105@gmail.com>
2025-09-18 15:00:10 +0700

    add new constant
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/backend/utils/constants.ts
+++ src/backend/utils/constants.ts
@@ -2,3 +2,7 @@ export const SALT_ROUNDS = 13;
 export const MAIL_TRANSPORT = Symbol("MAIL_TRANSPORT");
 export const REDIS_CLIENT = Symbol("REDIS_CLIENT");
 export const SIGNUP_STEPS = Symbol("SIGNUP_STEP");
+export const JWT_COOKIE_NAME = "jwt";
+export const REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+export const JWT_COOKIE_EXPIRES = 60 * 60 * 1000; // 1 hour
+export const REFRESH_TOKEN_COOKIE_EXPIRES = 30 * 24 * 60 * 60 * 1000; // 30 days
