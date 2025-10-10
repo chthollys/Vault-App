b845f0398dca2af4668cebeace32dee56eccbc3e
chthollys <jason130105@gmail.com>
2025-10-10 18:13:04 +0700

    fix(backend): set app trust proxy true
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { FRONTEND_URL, PORT } from "utils/env";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set("trust proxy", 1);
   app.enableCors({
     origin: FRONTEND_URL,
     credentials: true,
