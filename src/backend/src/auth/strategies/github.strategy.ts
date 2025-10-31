f3b9fce42eb055b9ffee167a98be036e659a3976
chthollys <jason130105@gmail.com>
2025-10-31 09:48:23 +0700

    ft(backend/guard): add github-auth-guard
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/backend/src/auth/strategies/github.strategy.ts
+++ src/backend/src/auth/strategies/github.strategy.ts
@@ -1,16 +1,46 @@
+import { BadGatewayException, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy } from "passport-github2";
+import { UsersService } from "src/users/users.service";
+import { API_HOST } from "utils/env";
+import { AccountsService } from "../accounts/accounts.service";
 
+@Injectable()
 export class GithubStrategy extends PassportStrategy(Strategy, "github") {
-  constructor() {
+  constructor(
+    private usersService: UsersService,
+    private accountService: AccountsService,
+  ) {
     super({
       clientID: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
-      callbackURL: process.env.FRONTEND_URL!,
+      callbackURL: `${API_HOST}/github/callback`,
     });
   }
 
-  validate(accessToken: string, refreshToken: string, profile: Profile) {
-    return {}
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    const email = profile.emails?.[0]?.value;
+    const providerAccountId = profile.id;
+    const image = profile.photos?.[0]?.value;
+    const name = profile.displayName || profile.username;
+
+    if (!email) {
+      throw new BadGatewayException("Github profile didn't provide an email");
+    }
+
+    const existingUser = await this.usersService.upsertByEmail(email, {
+      name,
+      image,
+    });
+
+    let account = await this.accountService.findOrCreateOAuthAccount({
+      provider: "github",
+      providerAccountId,
+      type: "oauth",
+      userId: existingUser.id,
+      tokens: { access_token: accessToken, refresh_token: refreshToken },
+    });
+
+    return account.user;
   }
 }
