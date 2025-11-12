5db12f327462fd4483119d5b541d76b710594891
chthollys <jason130105@gmail.com>
2025-11-12 22:49:23 +0700

    fix(hooks/useWishlist): enabled set to boolean instead of fixed value
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/frontend/src/app/hooks/useWishlist.ts
+++ src/frontend/src/app/hooks/useWishlist.ts
@@ -2,7 +2,7 @@ import { getWishlist } from "@/lib/api/client";
 import { useQuery } from "@tanstack/react-query";
 import { WISHLIST_BASEQUERYKEY as queryKey } from "@/lib/constants";
 
-export const useWishlist = (enabled: true) =>
+export const useWishlist = (enabled = true) =>
   useQuery({ queryKey, queryFn: getWishlist, enabled });
 
 export default useWishlist;
